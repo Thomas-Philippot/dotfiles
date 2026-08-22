fd8b2c0aace85cad6e575465c9dd866b54c502aa
Thomas <git@thomas-philippot.dev>
2026-08-22 11:57:14 +0200

    fix noctalia colors and add workspacerules

---
+++ .config/hypr/custom/workspacerules.lua
@@ -0,0 +1,4 @@
+hl.workspace_rule({ workspace = "1", monitor = "eDP-1", persistent = true, default_name = "web" })
+hl.workspace_rule({ workspace = "2", monitor = "eDP-1", persistent = true, default_name = "code" })
+-- hl.workspace_rule({ workspace = "3", monitor = "eDP-1", persistent = true, default_name = "chat" })
+-- hl.workspace_rule({ workspace = "4", monitor = "eDP-1", persistent = true, default_name = "game" })
